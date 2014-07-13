34b052ab565d9808e580414bfd3a84c40b9f9491
savoca <adeddo27@gmail.com>
2014-07-13 23:23:25 +0000

    LCD_KCAL: Fix mistake on out-of-range values
    This didn't cause any issues, but we don't need to reassign r/g/b again
    every time it passes through.

--- arch/arm/mach-msm/lge/mako/board-mako-display.c
+++ arch/arm/mach-msm/lge/mako/board-mako-display.c
@@ -279,15 +279,18 @@ void __init apq8064_mdp_writeback(struct memtype_reserve* reserve_table)
 #ifdef CONFIG_LCD_KCAL
 int kcal_set_values(int kcal_r, int kcal_g, int kcal_b)
 {
-	if (kcal_r > 255 || kcal_r < 0)
+	if (kcal_r > 255 || kcal_r < 0) {
 		kcal_r = kcal_r < 0 ? 0 : kcal_r;
 		kcal_r = kcal_r > 255 ? 255 : kcal_r;
-	if (kcal_g > 255 || kcal_g < 0)
+	}
+	if (kcal_g > 255 || kcal_g < 0) {
 		kcal_g = kcal_g < 0 ? 0 : kcal_g;
 		kcal_g = kcal_g > 255 ? 255 : kcal_g;
-	if (kcal_b > 255 || kcal_b < 0)
+	}
+	if (kcal_b > 255 || kcal_b < 0) {
 		kcal_b = kcal_b < 0 ? 0 : kcal_b;
 		kcal_b = kcal_b > 255 ? 255 : kcal_b;
+	}
 
 	kcal_value.red = kcal_r < g_kcal_min ? g_kcal_min : kcal_r;
 	kcal_value.green = kcal_g < g_kcal_min ? g_kcal_min : kcal_g;
