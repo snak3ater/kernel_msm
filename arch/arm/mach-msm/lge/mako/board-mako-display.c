3912657f28028cf3c1efa6e3da24cd5094d1c8b8
savoca <adeddo27@gmail.com>
2014-07-16 12:10:03 +0000

    s2d: turn off display if sweep < kcal_min, not 1

--- arch/arm/mach-msm/lge/mako/board-mako-display.c
+++ arch/arm/mach-msm/lge/mako/board-mako-display.c
@@ -374,9 +374,8 @@ void kcal_send_s2d(int set)
 		b = b + up_kcal;
 	}
 
-	if ((r < 1) && (g < 1) && (b < 1))
+	if ((r < g_kcal_min) && (g < g_kcal_min) && (b < g_kcal_min))
 		sweep2wake_pwrtrigger();
-		
 
 	kcal_set_values(r, g, b);
 	update_preset_lcdc_lut();
